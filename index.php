792611d00dee4d34036f02bfe981636b8134a0bf
Alessandro Licciardi <alessandro.licciardi@libero.it>
2024-12-12 21:14:30 +0100

    traccia 1

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+$num = 5;
+
+var_dump($num);
+
+
+$bool = true;
+
+var_dump($bool);
+
+
+
+$float = 0.8;
+
+
+var_dump($float);
+
+
+
+$string = "sono una stringa forse";
+
+var_dump($string);
+
+

90fafbcb59250d86321b84f831c21581e7b778b6
Alessandro Licciardi <alessandro.licciardi@libero.it>
2024-12-14 19:30:35 +0100

    2 esercizio

---
+++ esercitazione2.php
@@ -0,0 +1,20 @@
+
+$users = [
+
+['name' => 'Davide', 
+'surname' => 'Cariola', 
+'gender' => 'NB'];
+
+['name' => 'Gopvanna'
+'surname'=> 'Agrippina'
+'gender' => 'femmina'];
+
+['name' => 'Alberto'
+'surname => Mingardi
+'gender' => Maschio'];
+
+];
+
+foreach ($users as $key => $value) {
+echo "buongiorno sono $key"
+}
--- index.php
+++ index.php
@@ -42,3 +42,55 @@ $text8 = "tutto";
 
 
 var_dump($text1 . " " . $text2 . " " . $text3 . $text4 . " " . $text5 . " " . $text6 . " " . $text7 . " " . $text8);
+
+
+
+$words1 = [
+	'una',
+	67,
+	'vita',
+	'colle',
+	'mi',
+	'rosso',
+	[
+	'oscura',
+	'era',
+	89,
+		[
+		'mezzo',
+			[
+			'cammin',
+			'Nel',
+				[
+				'selva',
+				'la',
+					[
+					'via',
+					'una',
+					true,
+					]
+				],
+			]
+		],
+	'ritrovai',
+	'per'
+	],
+	'diritta'
+];
+
+
+$words2 = [
+ 'elemento1' => 25.89,
+ 'elemento2' => 'nostra',
+ 'elemento3' => [
+  'Virgilio',
+  'smarrita',
+  'ché'
+ ]
+];
+
+
+
+print_r($words1);
+
+echo $words1([6][3][1][1] . " " . [6][3][0]);
